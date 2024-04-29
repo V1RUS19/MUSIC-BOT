5e61f0de447ec6a1a654240c698f327314871a60
V1RUS19 <164151099+V1RUS19@users.noreply.github.com>
2024-04-29 11:32:56 +0300

    Update volume.js

--- musicCommands/volume.js
+++ musicCommands/volume.js
@@ -1,37 +1,37 @@
 
-константа { EmbedBuilder } = Требоват('дискорд.js');
-константа { getPlayer } = Требоват('./играть');
+const { EmbedBuilder } = require('discord.js');
+const { getPlayer } = require('./play');
 
-модуль.экспорт = {
-  Имя: 'объем',
-  Описание: 'Регулируйте громкость бота',
-  выполнять: асинхронный (сообщение, аргументы) => {
-    константа volume = анализироватьFloat(аргументы[0]);
+module.exports = {
+  name: 'volume',
+  description: 'Регулируйте громкость бота',
+  execute: async (message, args) => {
+    const volume = parseFloat(args[0]);
 
-    ЕСЛИ (isNaN(объем) || объем < 0 || объем > 100) {
-      возвращаться сообщение.Отвечать('❌ Укажите допустимый уровень громкости от 0 до 100.');
+    if (isNaN(volume) || volume < 0 || volume > 100) {
+      return message.reply('❌ Укажите допустимый уровень громкости от 0 до 100.');
     }
-    константа player = getPlayer();
+    const player = getPlayer();
 
-    ЕСЛИ (!игрок) {
-      возвращаться сообщение.Отвечать('❌ В настоящее время музыка не воспроизводится.');
+    if (!player) {
+      return message.reply('❌ В настоящее время музыка не воспроизводится.');
     }
-    константа resource = игрок.состояние.ресурс;
+    const resource = player.state.resource;
 
-    ЕСЛИ (!ресурс) {
-      возвращаться сообщение.Отвечать('❌ Аудиоресурс не найден.');
+    if (!resource) {
+      return message.reply('❌ Аудиоресурс не найден.');
     }
-    ресурс.объем.setVolume(объем / 100);
+    resource.volume.setVolume(volume / 100);
 
-    константа embed = новый EmbedBuilder()
+    const embed = new EmbedBuilder()
       .setColor('#2b71ec')
      .setAuthor({
-          Имя: 'Контроль громкости!',
-          значокURL: 'https://cdn.discordapp.com/attachments/1175488636033175602/1175488721546645624/volume.png?ex=656b6a2e&amp;is=6558f52e&amp;hm=8215d2f88ab073db1f3b6438c28fd73315ad7e 581bb54000dbb06fca387cecf7&',
-          URL: 'https://discord.gg/FUEHs7RCqz'
+          name: 'Volume Control!',
+          iconURL: 'https://cdn.discordapp.com/attachments/1175488636033175602/1175488721546645624/volume.png?ex=656b6a2e&is=6558f52e&hm=8215d2f88ab073db1f3b6438c28fd73315ad7e581bb54000dbb06fca387cecf7&',
+          url: 'https://discord.gg/FUEHs7RCqz'
         })
-      .setDescription(`**объем занят ${объем}%**`);
+      .setDescription(`**Громкость музыки на ${volume}%**`);
 
-    сообщение.Отвечать({ встраивает: [вставлять] });
+    message.reply({ embeds: [embed] });
   },
 };
