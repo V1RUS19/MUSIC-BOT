ec701de01c83a47e75ec65330a6d0d5a0049b438
V1RUS19 <164151099+V1RUS19@users.noreply.github.com>
2024-04-29 11:45:02 +0300

    Update queue.js

--- musicCommands/queue.js
+++ musicCommands/queue.js
@@ -13,7 +13,7 @@ module.exports = {
           iconURL: 'https://cdn.discordapp.com/attachments/1223544847047065662/1224631171766292500/9596-wrong.gif?ex=661e31a7&is=660bbca7&hm=0176645a3d582d6b93c8447a02cd7b1e7923b316212336fdc0b23b96b5e8ab4b&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-      .setDescription('**The Queue is currently empty consider adding songs.**')
+      .setDescription('**В настоящее время очередь пуста. Рассмотрите возможность добавления песен.**')
       .setColor('#ff0000');
     return message.reply({ embeds: [embed] });
     }
