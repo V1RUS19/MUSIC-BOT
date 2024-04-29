cf591d84b317634dcfdea4b6b52ed7bd5346b29f
V1RUS19 <164151099+V1RUS19@users.noreply.github.com>
2024-04-29 11:58:11 +0300

    Update help.js

--- basicCommands/help.js
+++ basicCommands/help.js
@@ -57,7 +57,7 @@ module.exports = {
         }
       )
       .setThumbnail(botUser.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
-      .setImage(`https://cdn.discordapp.com/attachments/1170652474563117138/1224660177039396876/MULTI_MUSIC_v1.5.png?ex=661e4cab&is=660bd7ab&hm=f7a542462fabaf9c1530ece5aa72597cff3ac032876bba46df5ddba7e122ea99&`);
+      .setImage(`https://media.discordapp.net/attachments/853597337057099776/1234428372667600896/kiberpank-74-foto-62.png?ex=6630b282&is=662f6102&hm=4e2933c8f154fd341e71979e7ac67251e4a574297d19d913a845e2be82dc81ea&=&format=webp&quality=lossless&width=550&height=325`);
 
  
     
